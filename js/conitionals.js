a93eb50d6a39ffcab6f03d2af57dbe6dea4c1e2c
Marcus Aguirre <marcus.a.aguirre@gmail.com>
2019-02-08 14:27:25 -0600

    added practice work during conditionals lecture

---
+++ js/conitionals.js
@@ -0,0 +1,14 @@
+"use strict";
+
+
+var yes = confirm("True or false?");
+
+// if(yes) {
+//     console.log("You said yes.");
+//
+// }else {
+//     console.log("You said no.");
+// }
+
+
+(yes) ? console.log("You said yes, ternary") : console.log("You said no, ternary");
