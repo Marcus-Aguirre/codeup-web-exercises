cbc763e4cad1cc3df60d386dceb09d5c68b468a5
Marcus Aguirre <marcus.a.aguirre@gmail.com>
2019-02-13 11:34:22 -0600

    small revisions

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,67 +1,67 @@
 "use strict";
 
 
-// function showMultiplicationTable(input){
-//     for(var i = 1; i <= 10; i++){
-//         console.log(input + " x " + i + " = " + input * i);
-//     }
-// }
-//
-// showMultiplicationTable(5);
+function showMultiplicationTable(input){
+    for(var i = 1; i <= 10; i++){
+        console.log(input + " x " + i + " = " + input * i);
+    }
+}
+
+showMultiplicationTable(5);
 
 
 
 
-// for (var i = 1; i <= 10; i++){
-//     var rand = Math.floor(Math.random() * 20) + 100;
-//     if(rand % 2 === 0){
-//         console.log(rand + " is even");
-//     }
-//     console.log(rand + " is odd");
-// }
+for (var i = 1; i <= 10; i++){
+    var rand = Math.floor(Math.random() * 20) + 100;
+    if(rand % 2 === 0){
+        console.log(rand + " is even");
+    }
+    console.log(rand + " is odd");
+}
 
 
 
 
-// for (var i = 100; i > 0; i -= 5){
-//     console.log(i);
-// }
+for (var i = 100; i > 0; i -= 5){
+    console.log(i);
+}
 
 
 
 
 
 
-//
-// function concatenateNumbers(input){
-//
-//     if (input === 1) {
-//         console.log("" + i);
-//     } else if (input === 2) {
-//         console.log("" + i + i);
-//     }else if (input === 3) {
-//         console.log("" + i + i + i);
-//     }else if (input === 4) {
-//         console.log("" + i + i + i + i);
-//     }else if (input === 5) {
-//         console.log("" + i + i + i + i + i);
-//     }else if (input === 6) {
-//         console.log("" + i + i + i + i + i + i);
-//     }else if (input === 7) {
-//         console.log("" + i + i + i + i + i + i + i);
-//     }else if (input === 8) {
-//         console.log("" + i + i + i + i + i + i + i + i);
-//     }else if (input === 9) {
-//         console.log("" + i + i + i + i + i + i + i + i + i);
-//     }
-// }
-//
-//
-// for(var i = 1; i <= 9; i++) {
-//
-//     concatenateNumbers(i);
-//
-// }
+
+function concatenateNumbers(input){
+
+    if (input === 1) {
+        console.log("" + i);
+    } else if (input === 2) {
+        console.log("" + i + i);
+    }else if (input === 3) {
+        console.log("" + i + i + i);
+    }else if (input === 4) {
+        console.log("" + i + i + i + i);
+    }else if (input === 5) {
+        console.log("" + i + i + i + i + i);
+    }else if (input === 6) {
+        console.log("" + i + i + i + i + i + i);
+    }else if (input === 7) {
+        console.log("" + i + i + i + i + i + i + i);
+    }else if (input === 8) {
+        console.log("" + i + i + i + i + i + i + i + i);
+    }else if (input === 9) {
+        console.log("" + i + i + i + i + i + i + i + i + i);
+    }
+}
+
+
+for(var i = 1; i <= 9; i++) {
+
+    concatenateNumbers(i);
+
+}
 
 
 
