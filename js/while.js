7c5b89f8bbee34e8b34bad180b16b5228ac969ed
Marcus Aguirre <marcus.a.aguirre@gmail.com>
2019-02-13 11:02:07 -0600

    created and worked on loops exercises

---
+++ js/while.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var i = 0;
+var x = 1;
+
+while(i < 16){
+    i++;
+    console.log(x *= 2);
+}
+
+////////////////////////////////
+
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+console.log("Total cones available to sell: " + allCones);
+
+do{
+    var cones_sold = Math.floor(Math.random() * 5) + 1;
+
+    if(allCones > cones_sold){
+        allCones -= cones_sold;
+        console.log(cones_sold + " cones sold. " + allCones + " remaining.");
+
+    }else if(allCones === cones_sold){
+        allCones -= cones_sold;
+        console.log(cones_sold + " cones sold. " + allCones + " remaining.");
+        console.log("All the cones have been sold.");
+        break;
+
+    }else if(allCones < cones_sold){
+        console.log("I don't have enough cones to sell you, sorry.");
+    }
+
+} while(allCones >= 0)
