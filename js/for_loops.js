53a3c96f0082ef26e5683c6586490135c6185e49
Marcus Aguirre <marcus.a.aguirre@gmail.com>
2019-02-25 13:26:49 -0600

    made small changes / refactored code

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,42 +1,61 @@
 "use strict";
 
 
-function showMultiplicationTable(input){
-    for(var i = 1; i <= 10; i++){
-        console.log(input + " x " + i + " = " + input * i);
-    }
-}
-
-showMultiplicationTable(5);
-
-
-
-
-for (var i = 1; i <= 10; i++){
-    var rand = Math.floor(Math.random() * 180) + 20;
-    if(rand % 2 === 0){
-        console.log(rand + " is even");
-    }
-    console.log(rand + " is odd");
-}
-
-
+// function showMultiplicationTable(input){
+//     for(var i = 1; i <= 10; i++){
+//         console.log(input + " x " + i + " = " + input * i);
+//     }
+// }
+//
+// showMultiplicationTable(5);
+//
+//
+//
+//
+// for (var i = 1; i <= 10; i++){
+//     var rand = Math.floor(Math.random() * 180) + 20;
+//     if(rand % 2 === 0){
+//         console.log(rand + " is even");
+//     }
+//     console.log(rand + " is odd");
+// }
 
 
 
 
-for(var i = 1; i <= 9; i++) {
-   console.log(("" + i).repeat(i));
-
-}
 
 
+// for(var i = 1; i <= 9; i++) {
+//    console.log(("" + i).repeat(i));
+//
+// }
+//
+//
+//
+//
+// for (var i = 100; i > 0; i -= 5){
+//     console.log(i);
+// }
 
 
-for (var i = 100; i > 0; i -= 5){
-    console.log(i);
-}
 
+
+
+
+
+// for (var i = 0; i < 101; i++) {
+//     if(i === 55){
+//         console.log("Thanks for all the fish. Goodbye");
+//         break
+//     } else if (i % 10 === 0){
+//         console.log("Skipping along");
+//         continue
+//     } else if (i % 2 === 0){
+//         console.log(i + " is even.");
+//     } else if (i % 2 === 1){
+//         console.log(i + " is odd.");
+//     }
+// }
 
 
 
