c5a181a15d0ab3f4fcd87ea176971d411154318d
Marcus-Aguirre <marcus.a.aguirre@gmail.com>
2019-06-21 11:45:45 -0500

    added tests of all functions to verify accuracy

--- js/JSPractice.js
+++ js/JSPractice.js
@@ -4,16 +4,30 @@
 function isOdd(number){
     return number % 2 === 1;
 }
+console.log(isOdd(5)); //TRUE
+console.log(isOdd(16)); // FALSE
+console.log(isOdd(26)); // FALSE
+
+
 
 // IS EVEN
 function isEven(number) {
     return number % 2 === 0;
 }
+console.log(isEven(6)); //TRUE
+console.log(isEven(15)); // FALSE
+console.log(isEven(24)); // TRUE
+
+
 
 // IS MULTIPLE OF VAR
 function isMultiple(number, multNum){
     return number % multNum === 0;
 }
+console.log(isMultiple(10, 2)); // TRUE
+console.log(isMultiple(15, 7)); // FALSE
+console.log(isMultiple(100, 5)); // TRUE
+
 
 
 // MINUS VAR IS ODD
@@ -21,11 +35,19 @@ function minusIsOdd(number, minusNum){
     var newNum = number - minusNum;
     return newNum % 2 === 1;
 }
+console.log(minusIsOdd(10, 2)); // FALSE
+console.log(minusIsOdd(10, 3)); // TRUE
+console.log(minusIsOdd(100, 52)); // FALSE
+
+
 
 // MINUS VAR IS EVEN
 function minusIsEven(number, minusNum){
     var newNum = number - minusNum;
     return newNum % 2 === 0;
 }
+console.log(minusIsEven(10, 2)); // TRUE
+console.log(minusIsEven(10, 3)); // FALSE
+console.log(minusIsEven(100, 52)); // TRUE
 
 
