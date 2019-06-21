5850091d9a38cb1814351903df35a717c8241a11
Marcus-Aguirre <marcus.a.aguirre@gmail.com>
2019-06-21 11:30:50 -0500

    beginning JavaSript practice of fundamentals

---
+++ js/JSPractice.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// IS ODD
+function isOdd(number){
+    return number % 2 === 1;
+}
+
+// IS EVEN
+function isEven(number) {
+    return number % 2 === 0;
+}
+
+// IS MULTIPLE OF VAR
+
+// MINUS VAR IS ODD
+
+// MINUS VAR IS EVEN
+
